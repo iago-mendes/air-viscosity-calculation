9acdb8f3a4619f4b46cf664ff8bc0646bc2a79a0
Iago <iagobrazmendes@gmail.com>
2021-03-18 16:14:00 -0300

    improve script

--- 1-melhor-ajuste/n2_separa_intervalos.m
+++ 1-melhor-ajuste/n2_separa_intervalos.m
@@ -10,34 +10,36 @@ t_5min = t;
 x_end = x;
 t_end = t;
 
+div = 600;
+
 for i=length(x) : -1 : 1
-	if t_10s(i) <= 243.4327999 || t_10s(i) >= 253.4327999
+	if t_10s(i) <= div || t_10s(i) >= div + 10
 		x_10s(i) = [];
 		t_10s(i) = [];
 	end
-	if t_1min(i) <= 243.4327999 || t_1min(i) >= 303.4327999
+	if t_1min(i) <= div || t_1min(i) >= div + 60
 		x_1min(i) = [];
 		t_1min(i) = [];
 	end
-	if t_5min(i) <= 243.4327999 || t_5min(i) >= 543.4327999
+	if t_5min(i) <= div || t_5min(i) >= div + 5*60
 		x_5min(i) = [];
 		t_5min(i) = [];
 	end
-	if t_end(i) <= 243.4327999
+	if t_end(i) <= div
 		x_end(i) = [];
 		t_end(i) = [];
 	end
 end
 
 for i=1 : length(t_10s)
-	t_10s(i) = t_10s(i) - 243.4327999;
+	t_10s(i) = t_10s(i) - div;
 end
 for i=1 : length(t_1min)
-	t_1min(i) = t_1min(i) - 243.4327999;
+	t_1min(i) = t_1min(i) - div;
 end
 for i=1 : length(t_5min)
-	t_5min(i) = t_5min(i) - 243.4327999;
+	t_5min(i) = t_5min(i) - div;
 end
 for i=1 : length(t_end)
-	t_end(i) = t_end(i) - 243.4327999;
+	t_end(i) = t_end(i) - div;
 end
